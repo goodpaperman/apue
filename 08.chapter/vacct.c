5d20c516af85b81ad8d40b88fe48c9ee94a02099
yunh glodon centos <yunhai@bogon.(none)>
2017-11-03 02:09:55 -0700

    new version struct

--- 08.chapter/vacct.c
+++ 08.chapter/vacct.c
@@ -26,19 +26,24 @@ int main (void)
     int tck = sysconf (_SC_CLK_TCK); 
     printf ("clock tick: %d\n", tck); 
 
-    struct acct acdata; 
+    struct acct_v3 acdata; 
+    printf ("sizeof (acct) = %d\n", sizeof (acdata)); 
+
     FILE        *fp; 
     if ((fp = fopen (ACCTFILE, "r")) == NULL)
 	err_sys ("can't open %s", ACCTFILE); 
+
     while (fread (&acdata, sizeof (acdata), 1, fp) == 1) 
     {
 	//printf ("%-*.*s e = %6ld, chars = %7ld, "
-	printf ("%-10.10s e = %6ld, chars = %7ld, "
+	printf (//"%-10.10s e = %6ld, chars = %7ld, "
+	        "%-10.10s e = %-6.2f, chars = %7ld, "
 		"stat = %3u: %c %c %c %c\n", 
 		//sizeof (acdata.ac_comm), 
 		//sizeof (acdata.ac_comm), 
 		acdata.ac_comm, 
-		compt2ulong (acdata.ac_etime), 
+		//compt2ulong (acdata.ac_etime), 
+		acdata.ac_etime, 
 		compt2ulong (acdata.ac_io), 
 		(unsigned char) acdata.ac_exitcode, 
 #ifdef ACORE
