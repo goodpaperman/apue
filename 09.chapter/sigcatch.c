fb0004ebb099df0f48d9c07d0fff7202271d3b92
yunh <haihai107@126.com>
2018-02-06 18:49:27 -0800

    fork children and see signal effect on them.

--- 09.chapter/sigcatch.c
+++ 09.chapter/sigcatch.c
@@ -2,20 +2,40 @@
 #include <errno.h> 
 #include "../apue.h" 
 
+int g_quit = 0; 
 void sighandler (int signo)
 {
-    printf ("catch %d\n", signo); 
+    printf ("%d catch %d\n", getpid(), signo); 
+    if (signo == SIGINT)
+        g_quit = 1; 
+
+    // PERSIST IT !
+    signal (signo, sighandler); 
 }
 
 int main (int argc, char *argv[])
 {
-    //int ret = 0; 
+    pid_t pid = 0; 
     signal (SIGINT, sighandler); 
     signal (SIGQUIT, sighandler); 
-    while (1)
+    for (int i=1; i<argc; ++ i)
+    {
+        pid = fork (); 
+        if (pid == 0)
+        {
+            // children
+            execvp ("./sigcatch", argv+1); 
+            exit (-1); 
+        }
+        else 
+            printf ("create children %d\n", pid); 
+    }
+
+    while (!g_quit)
     {
         sleep (1); 
     }
 
+    printf ("%d quit\n", getpid ()); 
     return 0; 
 }
