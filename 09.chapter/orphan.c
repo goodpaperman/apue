435c583de39bc24ca9189faf9e5b4a0ee354f7d9
yunh <haihai107@126.com>
2018-05-11 03:09:30 -0700

    more test.

--- 09.chapter/orphan.c
+++ 09.chapter/orphan.c
@@ -13,24 +13,54 @@ static void pr_ids (char const* name)
     fflush (stdout); 
 }
 
+#define N 5
+#define ALL_STOP
+//#define ALL_SLEEP
+
 int main (void)
 {
     char c; 
     pid_t pid; 
     pr_ids ("parent"); 
-    if ((pid = fork ()) < 0) { 
-        err_sys ("fork error"); 
-    } else if (pid > 0)
+    for (int i=0; i<N; ++ i)
     {
-        sleep (5); 
-        exit (0); 
-    } else {
-        pr_ids ("child"); 
-        signal (SIGHUP, sig_hup); 
-        kill (getpid (), SIGTSTP); 
-        pr_ids ("child"); 
-        if (read (STDIN_FILENO, &c, 1) != 1)
-            printf ("read error from controlling TTY, errno = %d\n", errno); 
-        exit (0); 
+        if ((pid = fork ()) < 0) { 
+            err_sys ("fork error"); 
+        } else if (pid == 0) {
+            char name[128] = { 0 }; 
+            sprintf (name, "%s%d", "child", i+1); 
+            pr_ids (name); 
+            signal (SIGHUP, sig_hup); 
+            if (i%2 == 0)
+            {
+#ifdef ALL_STOP
+                printf ("before stop self: %d\n", getpid ()); 
+                kill (getpid (), SIGTSTP); 
+#else 
+                printf ("before sleep self: %d\n", getpid ()); 
+                sleep (6); 
+#endif
+            }
+            else 
+            {
+#ifdef ALL_SLEEP
+                printf ("before sleep self: %d\n", getpid ()); 
+                sleep (6); 
+#else
+                printf ("before stop self: %d\n", getpid ()); 
+                kill (getpid (), SIGTSTP); 
+#endif 
+            }
+
+            pr_ids (name); 
+            if (read (STDIN_FILENO, &c, 1) != 1)
+                printf ("read error from controlling TTY, errno = %d\n", errno); 
+
+            exit (0); 
+        }
     }
+
+    sleep (5); 
+    exit (0); 
+    return 0; 
 }
