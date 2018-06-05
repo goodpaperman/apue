6633d0c2647eac80c600b13be13422e7df7f7248
yunh <haihai107@126.com>
2018-06-05 03:02:59 -0700

    to generate SIGABRT & SIGPIPE

--- 10.chapter/testsig.c
+++ 10.chapter/testsig.c
@@ -1,6 +1,11 @@
 #include "../apue.h" 
 
-int main ()
+void test_abrt ()
+{
+    abort (); 
+}
+
+void test_fpe ()
 {
     int i = 1; 
     double j = 0.0; 
@@ -8,5 +13,46 @@ int main ()
     printf ("k = %.2f\n", k); 
     k = i / 0; 
     printf ("k = %.2f\n", k); 
+}
+
+//#define MAXLINE 80
+void test_pipe ()
+{
+    int fd[2] = { 0 }; 
+    if (pipe(fd) < 0)
+        err_sys ("pipe error");
+
+    pid_t pid; 
+    if ((pid = fork ()) < 0)
+        err_sys ("fork error"); 
+    else if (pid > 0)
+    {
+        close (fd[0]); 
+        write (fd[1], "hello world\n", 12); 
+        sleep (1); 
+        write (fd[1], "hello world\n", 12); 
+        sleep (1); 
+        write (fd[1], "hello world\n", 12); 
+    }
+    else 
+    {
+        close (fd[1]); 
+        char line [MAXLINE] = { 0 }; 
+        int n = read (fd[0], line, MAXLINE);
+        write (STDOUT_FILENO, line, n); 
+        n = read (fd[0], line, MAXLINE); 
+        write (STDOUT_FILENO, line, n); 
+    }
+}
+
+int main ()
+{
+#if 0
+    test_abrt (); 
+#elif 0 
+    test_fpe (); 
+#else 
+    test_pipe (); 
+#endif 
     return 0; 
 }
