13b4580ddf7e03bc24312f4c36cc13e5240f1963
yunh <haihai107@126.com>
2018-07-09 18:09:02 -0700

    to test signal block and unblock/ignore.

---
+++ 10.chapter/blocksig.c
@@ -0,0 +1,44 @@
+#include "../apue.h" 
+#include <signal.h>
+
+static void sig_int (int signo)
+{
+    printf ("caught SIGINT\n"); 
+#if 0
+    if (signal (SIGINT, SIG_DFL) == SIG_ERR)
+#else 
+    if (signal (SIGINT, SIG_IGN) == SIG_ERR)
+#endif
+        err_sys ("can't reset SIGINT"); 
+}
+
+int main (int argc, char *argv[])
+{
+    sigset_t newmask, oldmask, pendmask; 
+    if (signal (SIGINT, sig_int) == SIG_ERR)
+        err_sys ("can't catch SIGINT"); 
+
+    sigemptyset (&newmask); 
+    sigaddset (&newmask, SIGINT); 
+    if (sigprocmask (SIG_BLOCK, &newmask, &oldmask) < 0)
+        err_sys ("SIG_BLOCK error"); 
+
+    sleep (5); 
+    if (sigpending (&pendmask) < 0)
+        err_sys ("sigpending error"); 
+    if (sigismember (&pendmask, SIGINT))
+        printf ("SIGINT pending\n"); 
+
+#if 0
+    if (sigprocmask (SIG_SETMASK, &oldmask, NULL) < 0)
+        err_sys ("SIG_SETMASK error"); 
+#else 
+    if (signal (SIGINT, SIG_IGN) == SIG_ERR)
+        err_sys ("can't ignore SIGINT"); 
+#endif 
+
+    printf ("SIGINT unblocked\n"); 
+    sleep (5); 
+    exit (0); 
+}
+
