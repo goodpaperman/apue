9f6efa5a0a2d2e8bf931aff47370fe087f4f6180
yunh <haihai107@126.com>
2018-07-11 19:02:40 -0700

    right test, SIG_IGN does NOT unblock any pending signals, APUE says wrong !!

--- 10.chapter/blocksig.c
+++ 10.chapter/blocksig.c
@@ -1,13 +1,17 @@
 #include "../apue.h" 
 #include <signal.h>
 
+#define IGNORE2CATCH
+
 static void sig_int (int signo)
 {
     printf ("caught SIGINT\n"); 
 #if 0
     if (signal (SIGINT, SIG_DFL) == SIG_ERR)
-#else 
+#elif 0 
     if (signal (SIGINT, SIG_IGN) == SIG_ERR)
+#else
+    if (signal (SIGINT, sig_int) == SIG_ERR)
 #endif
         err_sys ("can't reset SIGINT"); 
 }
@@ -15,17 +19,24 @@ static void sig_int (int signo)
 int main (int argc, char *argv[])
 {
     sigset_t newmask, oldmask, pendmask; 
-#if 1
+#ifdef IGNORE2CATCH
     if (signal (SIGINT, SIG_IGN) == SIG_ERR)
 #else 
     if (signal (SIGINT, sig_int) == SIG_ERR)
 #endif
-        err_sys ("can't catch SIGINT"); 
+        err_sys ("can't catch/ignore SIGINT"); 
 
     sigemptyset (&newmask); 
     sigaddset (&newmask, SIGINT); 
     if (sigprocmask (SIG_BLOCK, &newmask, &oldmask) < 0)
         err_sys ("SIG_BLOCK error"); 
+
+#ifdef IGNORE2CATCH 
+    if (signal (SIGINT, sig_int) == SIG_ERR)
+#else 
+    if (signal (SIGINT, SIG_IGN) == SIG_ERR)
+#endif
+        err_sys ("can't catch/ignore SIGINT"); 
 
     sleep (5); 
     if (sigpending (&pendmask) < 0)
@@ -33,20 +44,13 @@ int main (int argc, char *argv[])
     if (sigismember (&pendmask, SIGINT))
         printf ("SIGINT pending\n"); 
 
-#if 1 
-    if (signal (SIGINT, sig_int) == SIG_ERR)
-        err_sys ("can't catch SIGINT"); 
-#endif
-
-#if 1
+#ifdef IGNORE2CATCH
     if (sigprocmask (SIG_SETMASK, &oldmask, NULL) < 0)
         err_sys ("SIG_SETMASK error"); 
-#else 
-    if (signal (SIGINT, SIG_IGN) == SIG_ERR)
-        err_sys ("can't ignore SIGINT"); 
-#endif 
 
     printf ("SIGINT unblocked\n"); 
+#endif
+
     sleep (5); 
     if (sigpending (&pendmask) < 0)
         err_sys ("sigpending error"); 
