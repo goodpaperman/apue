b482533365d28082e2cbbf488cc5be0b89ce2c95
yunh <haihai107@126.com>
2018-07-12 17:43:02 -0700

    to test kill returned after signal handler called.

--- 10.chapter/killsig.c
+++ 10.chapter/killsig.c
@@ -10,15 +10,33 @@ void sigint (int signo)
 
 int main (int argc, char *argv[])
 {
-    if (argc < 3)
+    int ret = 0; 
+    pid_t pid = -1; 
+    int signo = 0; 
+    if (argc < 2)
     {
         printf ("Usage: killsig pid signo\n"); 
         return -1; 
     }
+    else if (argc == 2)
+    {
+        pid = 0; 
+        signo = atoi (argv[1]); 
+    }
+    else 
+    {
+        pid = atoi(argv[1]); 
+        signo = atoi(argv[2]); 
+    }
 
-    int signo = atoi(argv[2]); 
     signal (signo, sigint); 
-    int ret = kill (atoi(argv[1]), signo); 
-    printf ("kill return %d, errno %d\n", ret, errno); 
+#if 0
+    if (argc == 2)
+        ret = raise (signo); 
+    else 
+#endif
+        ret = kill (pid, signo); 
+
+    printf ("kill/raise return %d, errno %d\n", ret, errno); 
     return 0; 
 }
