36380c6aa215b5f7d2b137662ad30efdde2ad658
yunh <haihai107@126.com>
2018-08-13 17:46:47 -0700

    right way to do system()

--- apue.c
+++ apue.c
@@ -190,6 +190,7 @@ void tell_buf (char const* name, FILE* fp)
   printf ("discriptor is %d\n\n", fileno (fp)); 
 }
 
+#if 0
 int apue_system (const char *cmdstring)
 {
     pid_t pid; 
@@ -224,6 +225,68 @@ int apue_system (const char *cmdstring)
 
     return (status); 
 }
+#else
+int apue_system (const char *cmdstring)
+{
+    pid_t pid; 
+    int status; 
+    struct sigaction ignore, saveintr, savequit; 
+    sigset_t chldmask, savemask; 
+    if (cmdstring == NULL)
+        return 1; 
+
+    ignore.sa_handler = SIG_IGN; 
+    sigemptyset (&ignore.sa_mask); 
+    ignore.sa_flags = 0; 
+    if (sigaction (SIGINT, &ignore, &saveintr) < 0)
+        return -1; 
+    if (sigaction (SIGQUIT, &ignore, &savequit) < 0)
+        return -1; 
+
+    sigemptyset (&chldmask); 
+    sigaddset (&chldmask, SIGCHLD); 
+    if (sigprocmask (SIG_BLOCK, &chldmask, &savemask) < 0)
+        return -1; 
+
+    if ((pid = fork ()) < 0)
+    {
+        status = -1; 
+    }
+    else if (pid == 0) 
+    {
+        sigaction (SIGINT, &saveintr, NULL); 
+        sigaction (SIGQUIT, &savequit, NULL); 
+        sigprocmask (SIG_SETMASK, &savemask, NULL); 
+
+        printf ("before calling shell: %s\n", cmdstring); 
+        execl ("/bin/sh", "sh", "-c", cmdstring, (char *)0); 
+        _exit (127); 
+    }
+    else 
+    {
+        //sleep (10); 
+        while (waitpid (pid, &status, 0) < 0)
+        {
+            if (errno != EINTR)
+            {
+                printf ("wait cmd failed, errno = %d\n", errno); 
+                status = -1; 
+                break; 
+            }
+        }
+        printf ("wait cmd, status = %d\n", status); 
+    }
+
+    if (sigaction (SIGINT, &saveintr, NULL) < 0)
+        return -1; 
+    if (sigaction (SIGQUIT, &savequit, NULL) < 0)
+        return -1; 
+    if (sigprocmask (SIG_SETMASK, &savemask, NULL) < 0)
+        return -1; 
+
+    return (status); 
+}
+#endif
 
 void pr_exit (int status)
 {
