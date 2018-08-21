fa20b6ae66d68fcab31cd65371c0d981de98255f
yunh <haihai107@126.com>
2018-08-20 18:01:13 -0700

    to test signal to string.

---
+++ 10.chapter/signame.c
@@ -0,0 +1,61 @@
+#include "../apue.h" 
+#include <signal.h>
+#include <string.h> 
+
+//extern char *sys_siglist[]; 
+
+void test_siglist ()
+{
+    for (int i=0; i<_NSIG; ++ i)
+        printf ("%s: %d\n", sys_siglist[i], i); 
+
+    printf ("\n"); 
+}
+
+void test_psig ()
+{
+    char msg[64] = { 0 }; 
+    for (int i=0; i<_NSIG; ++ i)
+    {
+        sprintf (msg, "%d ", i); 
+        psignal (i, msg); 
+        //printf ("%s", msg); 
+    }
+
+    printf ("\n"); 
+}
+
+void test_strsig ()
+{
+    for (int i=0; i<_NSIG; ++ i)
+    {
+        printf ("[%d] %s\n", i, strsignal (i)); 
+    }
+
+    printf ("\n"); 
+}
+
+#ifdef SOLARIS
+void test_conv ()
+{
+    int signo; 
+    char str[SIG2STR_MAX] = { 0 }; 
+    for (int i=0; i<_NSIG; ++ i)
+    {
+        sig2str (i, str); 
+        str2sig (str, &signo); 
+        printf ("[%d][%d] %s\n", i, signo, str); 
+    }
+}
+#endif
+
+int main ()
+{
+    test_siglist (); 
+    test_psig (); 
+    test_strsig (); 
+#ifdef SOLARIS
+    test_conv (); 
+#endif
+    return 0; 
+}
