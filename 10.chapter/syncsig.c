ca01f04022a82327637756db892f47cc8f8005ff
yunh <haihai107@126.com>
2018-08-26 18:09:55 -0700

    use open/read/write OK, fix bug in fopen/fread/fwrite (file mode not corret)

--- 10.chapter/syncsig.c
+++ 10.chapter/syncsig.c
@@ -2,6 +2,7 @@
 #include <sys/wait.h>
 #include <errno.h>
 
+#define USE_FGETS
 static volatile sig_atomic_t sigflag; 
 static sigset_t newmask, oldmask, zeromask; 
 
@@ -46,31 +47,63 @@ int read_increase_write ()
 {
     char *ptr = NULL; 
     int n = 0, ret = 0, err = 0; 
-    FILE* fp = fopen ("sync.txt", "w+"); 
+#ifdef USE_FGETS
+    FILE* fp = fopen ("sync.txt", "r"); 
     if (fp)
+#else
+    int fd = open ("sync.txt", O_CREAT | O_RDWR, 0622); 
+    if (fd != -1)
+#endif
     {
         char buf[64] = { 0 }; 
         //fread (buf, sizeof(buf)-1, 1, fp); 
         do
         {
             errno = 0; 
-            ptr = fgets (buf, sizeof(buf), fp); 
+#ifdef USE_FGETS
+            //ptr = fgets (buf, sizeof(buf), fp); 
+            ret = fread (buf, 1, sizeof(buf), fp); 
+        //    err = errno; 
+        //    printf ("read %s, errno %d\n", ptr, err); 
+        //} while (ptr == NULL && err == EINTR); 
+#else 
+            ret = read (fd, buf, sizeof(buf)); 
+#endif
             err = errno; 
-            printf ("read %p, errno %d\n", ptr, err); 
-        } while (ptr == NULL && err == EINTR); 
+            printf ("read %d, errno %d\n", ret, err); 
+        } while (ret == -1 && err == EINTR); 
 
         n = atoi (buf); 
+        memset (buf, 0, sizeof(buf)); 
         sprintf (buf, "%d", ++n); 
         ////printf ("write %s\n", buf); 
         //fwrite (buf, strlen (buf), 1, fp); 
         
+#ifdef USE_FGETS
+        fp = freopen ("sync.txt", "w", fp); 
+        if (!fp)
+            err_sys ("freopen"); 
+#endif
+        
         do 
         {
             errno = 0; 
-            ret = fputs (buf, fp); 
+#ifdef USE_FGETS
+            //if (fseek (fp, 0, SEEK_SET) == -1)
+            //    err_sys ("fseek"); 
+
+            //ret = fputs (buf, fp); 
+            ret = fwrite (buf, 1, sizeof(buf), fp); 
+#else 
+            if (lseek (fd, 0, SEEK_SET) == -1)
+                err_sys ("lseek"); 
+
+            ret = write (fd, buf, sizeof(buf)); 
+#endif
             err = errno; 
             printf ("write %d, errno %d\n", ret, err); 
-        } while (ret == 0 && err == EINTR); 
+        } while (ret == -1 && err == EINTR); 
+
         printf ("[%d] read %d, write %d\n", getpid (), n-1, n); 
     }
     else 
@@ -79,7 +112,11 @@ int read_increase_write ()
         return -1; 
     }
 
+#ifdef USE_FGETS
     fclose (fp); 
+#else
+    close (fd); 
+#endif
     return n; 
 }
 
