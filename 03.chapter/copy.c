5565f91b7f0eca6cf567b9b75f1dbec316a4ee95
yunh <haihai107@126.com>
2018-08-29 18:13:56 -0700

    to test SIGXFSZ when write

--- 03.chapter/copy.c
+++ 03.chapter/copy.c
@@ -2,26 +2,39 @@
 
 //#define BUFFSIZE 4096
 
+void sig_xfsz (int signo)
+{
+    printf ("catch SIGXFSZ\n"); 
+    exit (-1); 
+}
+
 int main (int argc, char *argv[])
 {
   int n; 
   int bufsize = 4096; 
-  if (argc > 0)
+  if (argc > 1)
     bufsize = atoi (argv[1]); 
 
 #ifdef USE_O_SYNC
   set_fl (STDOUT_FILENO, O_SYNC); 
 #endif 
 
+  signal (SIGXFSZ, sig_xfsz); 
+
   //printf ("bufsize = %d", bufsize); 
   char *buf = (char *)malloc (bufsize); 
   while ((n = read (STDIN_FILENO, buf, bufsize)) > 0)
   {
-    if (write (STDOUT_FILENO, buf, n) != n)
+    int m = write (STDOUT_FILENO, buf, n); 
+    if (m != n)
       err_sys ("write error"); 
+    else 
+      fprintf (stderr, "write %d\n", m); 
+
 #ifdef USE_DATASYNC
     fdatasync (STDOUT_FILENO); 
 #endif 
+
 #ifdef USE_SYNC
     fsync (STDOUT_FILENO); 
 #endif
