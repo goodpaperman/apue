d2a94bac40ea71c1770bed979b2915cdb9d496a1
yunh <haihai107@126.com>
2018-09-03 17:55:24 -0700

    to see SIGXFSZ (only catch this signal when write 0 returned)

--- 03.chapter/copy.c
+++ 03.chapter/copy.c
@@ -1,10 +1,11 @@
 #include "../apue.h"
 
 //#define BUFFSIZE 4096
+#define USE_STDFILE
 
 void sig_xfsz (int signo)
 {
-    printf ("catch SIGXFSZ\n"); 
+    fprintf (stderr, "catch SIGXFSZ\n"); 
     exit (-1); 
 }
 
@@ -23,24 +24,38 @@ int main (int argc, char *argv[])
 
   //printf ("bufsize = %d", bufsize); 
   char *buf = (char *)malloc (bufsize); 
-  while ((n = read (STDIN_FILENO, buf, bufsize)) > 0)
+
+#ifdef USE_STDFILE
+  int fd_in = STDIN_FILENO; 
+  int fd_out = STDOUT_FILENO; 
+#else
+  int fd_in = open ("copy", O_RDONLY, 0644); 
+  if (fd_in == -1)
+      err_sys ("open in file error"); 
+
+  int fd_out = open ("copy2", O_WRONLY | O_CREAT, 0644); 
+  if (fd_out == -1)
+      err_sys ("open out file error"); 
+#endif
+
+  while ((n = read (fd_in, buf, bufsize)) > 0)
   {
-    int m = write (STDOUT_FILENO, buf, n); 
-    if (m != n)
-      err_sys ("write error"); 
+    int m = write (fd_out, buf, n); 
+    if (m == 0)
+      err_sys ("write error, n = %d", n); 
     else 
       fprintf (stderr, "write %d\n", m); 
 
 #ifdef USE_DATASYNC
-    fdatasync (STDOUT_FILENO); 
+    fdatasync (fd_out); 
 #endif 
 
 #ifdef USE_SYNC
-    fsync (STDOUT_FILENO); 
+    fsync (fd_out); 
 #endif
 
-    if (n < 0)
-      err_sys ("read error"); 
+    //if (n < 0)
+    //  err_sys ("read error"); 
   }
 
   free(buf); 
