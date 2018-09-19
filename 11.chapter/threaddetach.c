087c9b738a8d18e9d1a2438ca5e9a76d54ec2db0
yunh <haihai107@126.com>
2018-09-18 17:53:37 -0700

    test detached thread cann't join

---
+++ 11.chapter/threaddetach.c
@@ -0,0 +1,60 @@
+#include "../apue.h" 
+#include <pthread.h> 
+#include <error.h> 
+
+void* thr_fun (void *arg)
+{
+    printf ("this is thread %lu (0x%x)\n", pthread_self (), pthread_self ()); 
+    sleep (1); 
+
+#if 0
+    pthread_exit ((void *)pthread_self ()); 
+#endif 
+
+    sleep (1); 
+    printf ("exit from child thread\n"); 
+    return (void*) 24; 
+}
+
+int main ()
+{
+    int err = 0; 
+    pthread_t tid; 
+    pthread_attr_t att; 
+    
+    err = pthread_attr_init (&att); 
+    if (err != 0)
+        err_quit ("attr init failed\n"); 
+
+    err = pthread_attr_setdetachstate (&att, 1); 
+    if (err != 0)
+        err_quit ("set detach state failed\n"); 
+
+    err = pthread_create (&tid, &att, thr_fun, NULL); 
+    if (err != 0)
+        err_quit ("can't create thread: %s\n", strerror(err)); 
+
+    err = pthread_attr_destroy (&att); 
+    if (err != 0)
+        err_quit ("attr destroy failed\n"); 
+
+    sleep (1); 
+
+#if 0
+    err = pthread_cancel (tid); 
+    if (err != 0)
+        err_quit ("can't cancel thread: %s\n", strerror (err)); 
+    else 
+        printf ("cancel child\n"); 
+#endif 
+
+    void *status = 0; 
+    err = pthread_join (tid, &status); 
+    if (err != 0)
+        printf ("can't join thread: %s\n", strerror (err)); 
+    else 
+        printf ("wait child, status = 0x%x\n", status); 
+
+    printf ("main thread exit..\n"); 
+    return 42; 
+}
