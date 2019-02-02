7f1949df4e3fe432d4203e0cf51b2f1ed9edf761
yunhai <haihai107@126.com>
2019-02-02 13:26:11 +0800

    add filelock to test file locks using command.

---
+++ 14.chapter/filelock.c
@@ -0,0 +1,97 @@
+#include "../apue.h" 
+#include <fcntl.h> 
+#include <strings.h> 
+#include <errno.h> 
+
+void usage ()
+{
+    printf (
+      "Usage:\n"
+      "action: LOCK/LOCKWAIT/UNLOCK/TESTLOCK\n"
+      "type: READ/WRITE\n"
+      "whence: SET/CUR/END\n"
+      "filelock path action type offset whence len\n"
+      ); 
+    exit (-1); 
+}
+
+void dump_lock (struct flock *lck)
+{
+  printf ("lock detail:\n"); 
+  printf ("type %s\n", lck->l_type == F_RDLCK ? "rdlock" : (lck->l_type == F_WRLCK ? "wrlock" : "unlock") ); 
+  printf ("offset %u\n", lck->l_start); 
+  printf ("whence %s\n", lck->l_whence == SEEK_SET ? "set" : (lck->l_whence == SEEK_CUR ? "curr" : "end")); 
+  printf ("len %u\n", lck->l_len); 
+}
+
+int main (int argc, char *argv[])
+{
+  if (argc != 7)
+    usage (); 
+
+  off_t offset = atoi(argv[4]); 
+  off_t len = atoi(argv[6]); 
+  int whence = 0; 
+  if (strcasecmp (argv[5], "SET") == 0)
+    whence = SEEK_SET; 
+  else if (strcasecmp (argv[5], "CUR") == 0)
+    whence = SEEK_CUR; 
+  else if (strcasecmp (argv[5], "END") == 0)
+    whence = SEEK_END; 
+  else
+    usage (); 
+
+  int type = 0; 
+  if (strcasecmp (argv[3], "READ") == 0)
+    type = F_RDLCK; 
+  else if (strcasecmp (argv[3], "WRITE") == 0)
+    type = F_WRLCK; 
+  else 
+    usage (); 
+
+  int fd = open (argv[1], type == F_RDLCK ? O_RDONLY : O_WRONLY); 
+  if (fd == -1)
+    err_sys ("open %s failed", argv[1]); 
+
+  int action = 0; 
+  if (strcasecmp (argv[2], "LOCK") == 0)
+    action = F_SETLK; 
+  else if (strcasecmp (argv[2], "LOCKWAIT") == 0)
+    action = F_SETLKW; 
+  else if (strcasecmp (argv[2], "UNLOCK") == 0)
+  {
+    action = F_SETLK; 
+    type = F_UNLCK; 
+  }
+  else if (strcasecmp (argv[2], "TESTLOCK") == 0)
+    action = F_GETLK; 
+  else 
+    usage (); 
+
+  printf ("%lu start, action %s\n", getpid (), argv[2]); 
+  struct flock lock; 
+  lock.l_type = type; 
+  lock.l_start = offset; 
+  lock.l_whence = whence; 
+  lock.l_len = len; 
+  dump_lock (&lock); 
+  int ret = fcntl (fd, action, &lock); 
+  if (ret == -1)
+    err_sys ("fcntl failed, errno %d", errno); 
+
+  if (action == F_GETLK)
+  {
+    // dump result
+    if (lock.l_type == F_UNLCK)
+      printf ("no lock in destination found\n"); 
+    else 
+    {
+      printf ("find a lock owning by %lu\n", getpid()); 
+      dump_lock (&lock); 
+    }
+  }
+  else 
+    sleep (100); 
+
+  printf ("%lu exit\n", getpid ()); 
+}
--- apue.c
+++ apue.c
@@ -781,3 +781,31 @@ void daemonize (char const* cmd)
 #endif
 }
 
+int lock_reg (int fd, int cmd, int type, off_t offset, int whence, off_t len)
+{
+  struct flock lock; 
+  lock.l_type = type; 
+  lock.l_start = offset; 
+  lock.l_whence = whence; 
+  lock.l_len = len; 
+  return fcntl (fd, cmd, &lock); 
+}
+
+pid_t lock_test (int fd, int type, off_t *offset, int *whence, off_t *len)
+{
+  struct flock lock; 
+  lock.l_type = type; 
+  lock.l_start = *offset; 
+  lock.l_whence = *whence; 
+  lock.l_len = *len; 
+  if (fcntl(fd, F_GETLK, &lock) < 0)
+    err_sys ("fcntl error"); 
+
+  if (lock.l_type == F_UNLCK)
+    return 0; 
+
+  *offset = lock.l_start; 
+  *whence = lock.l_whence; 
+  *len = lock.l_len; 
+  return lock.l_pid; 
+}
