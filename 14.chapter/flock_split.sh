60748c10cbcbb4316e17d9dd2c28bcf339a62d8a
yunhai <haihai107@126.com>
2019-02-03 11:16:38 +0800

    to test l_len affects F_GETLCK sometimes.

--- 14.chapter/flock_split.sh
+++ 14.chapter/flock_split.sh
@@ -1,16 +1,18 @@
 #! /bin/sh
+len=1
+#len=50
 rm file.lck
 truncate -s 100 file.lck
 ./flocksplit file.lck 100 &
 sleep 1
-./filelock file.lck TESTLOCK WRITE 0 SET 1 &
-./filelock file.lck TESTLOCK WRITE 90 SET 1 &
-./filelock file.lck TESTLOCK WRITE 50 SET 1 &
+./filelock file.lck TESTLOCK WRITE 0 SET $len &
+./filelock file.lck TESTLOCK WRITE 90 SET $len &
+./filelock file.lck TESTLOCK WRITE 50 SET $len &
 sleep 2
-./filelock file.lck TESTLOCK WRITE 0 SET 1 &
-./filelock file.lck TESTLOCK WRITE 90 SET 1 &
-./filelock file.lck TESTLOCK WRITE 50 SET 1 &
+./filelock file.lck TESTLOCK WRITE 0 SET $len &
+./filelock file.lck TESTLOCK WRITE 90 SET $len &
+./filelock file.lck TESTLOCK WRITE 50 SET $len &
 sleep 2
-./filelock file.lck TESTLOCK WRITE 0 SET 1 &
-./filelock file.lck TESTLOCK WRITE 90 SET 1 &
-./filelock file.lck TESTLOCK WRITE 50 SET 1 &
+./filelock file.lck TESTLOCK WRITE 0 SET $len &
+./filelock file.lck TESTLOCK WRITE 90 SET $len &
+./filelock file.lck TESTLOCK WRITE 50 SET $len &
