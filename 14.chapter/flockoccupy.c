2542744d47f71307899aa20cd042285d9e5c016f
yunhai <haihai107@126.com>
2019-02-08 15:18:08 +0800

    a patch

--- 14.chapter/flockoccupy.c
+++ 14.chapter/flockoccupy.c
@@ -9,29 +9,43 @@ int main (int argc, char *argv[])
   pid_t pid; 
   char buf[5]; 
   struct stat statbuf; 
-  if (argc != 2) { 
-    fprintf (stderr, "usage: %s filename\n", argv[0]); 
+  if (argc != 3) { 
+    fprintf (stderr, "usage: %s filename truncate\n", argv[0]); 
     exit (1); 
   }
 
-  fd = open (argv[1], O_RDWR | O_CREAT, FILE_MODE); 
-  if (fd < 0)
-    err_sys ("open error"); 
+  int trunc = atoi (argv[2]); 
+  if (trunc)
+  {
+    fd = open (argv[1], O_RDWR | /*O_CREAT |*/ O_TRUNC, FILE_MODE); 
+    if (fd < 0)
+      err_sys ("open error"); 
 
-  if (fstat (fd, &statbuf) < 0)
-    err_sys ("fstat error"); 
+    printf ("truncate OK\n"); 
+    close (fd); 
+  }
+  else { 
+    fd = open (argv[1], O_RDWR | O_CREAT, FILE_MODE); 
+    if (fd < 0)
+      err_sys ("open error"); 
 
-  if (fchmod (fd, (statbuf.st_mode & ~S_IXGRP) | S_ISGID) < 0)
-    err_sys ("fchmod error"); 
+    if (fstat (fd, &statbuf) < 0)
+      err_sys ("fstat error"); 
 
-  if (write_lock (fd, 0, SEEK_SET, 0) < 0)
-    err_sys ("write_lock error"); 
+    if (fchmod (fd, (statbuf.st_mode & ~S_IXGRP) | S_ISGID) < 0)
+      err_sys ("fchmod error"); 
 
-  printf ("occupy file %s\n", argv[1]); 
-  sleep (60); 
+    if (write_lock (fd, 0, SEEK_SET, 0) < 0)
+      err_sys ("write_lock error"); 
 
-  if (fchmod (fd, statbuf.st_mode) < 0)
-    err_sys ("fchmod error"); 
+    printf ("occupy file %s\n", argv[1]); 
+    sleep (60); 
+
+    if (fchmod (fd, statbuf.st_mode) < 0)
+      err_sys ("fchmod error"); 
+
+    close (fd); 
+  }
 
   return 0; 
 }
