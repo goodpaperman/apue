02e40489d0ea39617365afa4ec054c54d57a4920
yunhai <haihai107@126.com>
2019-03-08 16:50:36 +0800

    to test isastream implemented by ioctl

--- 14.chapter/stream.c
+++ 14.chapter/stream.c
@@ -4,6 +4,12 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stropts.h>
+#include <string.h>
+
+int my_isastream (int fd)
+{
+    return (ioctl(fd, I_CANPUT, 0) != -1); 
+}
 
 int main (int argc, char *argv[])
 {
@@ -15,8 +21,12 @@ int main (int argc, char *argv[])
             continue; 
         }
 
+#if 0
         if (isastream (fd) == 0)
-            printf ("%s: not a stream, errno %d\n", argv[i], errno); 
+#else
+        if (my_isastream (fd) == 0)
+#endif
+            printf ("%s: not a stream, errno %d, msg %s\n", argv[i], errno, strerror(errno)); 
         else 
             printf ("%s: stream device\n", argv[i]); 
     }
