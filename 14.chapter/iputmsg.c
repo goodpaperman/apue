956a57909693b1b23e9e957f3f541651cc4a593a
yunhai <haihai107@126.com>
2019-03-20 10:26:42 +0800

    to test putmsg

---
+++ 14.chapter/iputmsg.c
@@ -0,0 +1,84 @@
+#include <stdio.h> 
+#include <stropts.h> 
+#include <errno.h> 
+#include <unistd.h> 
+#include <stdlib.h> 
+#include <string.h>
+
+#define BUFFSIZE 4096
+
+void set_wr_mode (int fd)
+{
+	int mode = 0; 
+	ioctl (fd, I_GWROPT, &mode); 
+	fprintf (stderr, "old write mode: %d\n", mode); 
+
+	mode = SNDZERO | SNDPIPE;
+	ioctl (fd, I_SWROPT, mode); 
+	fprintf (stderr, "new write mode: %d\n", mode); 
+}
+
+int main (void)
+{
+	int n, flag; 
+	char ctlbuf[BUFFSIZE], datbuf[BUFFSIZE]; 
+	struct strbuf ctl, dat; 
+	ctl.buf = ctlbuf; 
+	ctl.maxlen = BUFFSIZE; 
+	dat.buf = datbuf; 
+	dat.maxlen = BUFFSIZE; 
+	set_wr_mode (STDOUT_FILENO); 
+	for (;;) { 
+		flag = 0; 
+		if ((n = getmsg(STDIN_FILENO, &ctl, &dat, &flag)) < 0) {
+			printf ("getmsg error %d, %s\n", errno, strerror(errno)); 
+			exit(-1); 
+		}
+
+		fprintf (stderr, "flag = %d, ctl.len = %d, dat.len = %d\n", flag, ctl.len, dat.len); 
+
+		if (dat.len == 0)
+			exit (0); 
+		else if (dat.len > 0) {
+			//ctl.len = -1; 
+			//flag = 0;
+
+			// to test zero length send
+			if (dat.len == 1 && dat.buf[0] == '\n')
+				dat.len = 0; 
+#if 0
+			dat.len = -1; 
+#elif 0
+			dat.len = 0; 
+#elif 0
+			// warning: telnet reset
+			strcpy (ctl.buf, "hello world!"); 
+			ctl.len = strlen(ctl.buf); 
+#elif 0
+			// error: EINVAL
+			flag = RS_HIPRI; 
+#elif 0
+			// blocked, use Ctrl+C to exit
+			// if use Ctrl+D you will get telnet reset
+			strcpy (ctl.buf, "hello world!"); 
+			ctl.len = strlen(ctl.buf); 
+			flag = RS_HIPRI; 
+#elif 0
+			// blocked, use Ctrl+C to exit
+			// if use Ctrl+D you will get telnet reset
+			strcpy (ctl.buf, "hello world!"); 
+			ctl.len = strlen(ctl.buf); 
+			dat.len = -1; 
+			flag = RS_HIPRI; 
+#endif 
+			if (putmsg (STDOUT_FILENO, &ctl, &dat, flag) < 0){ 
+				printf ("putmsg error %d, %s\n", errno, strerror(errno)); 
+				exit (-1); 
+			}
+
+			fprintf (stderr, "flag = %d, ctl.len = %d, dat.len = %d\n", flag, ctl.len, dat.len); 
+		}
+	}
+
+	return 0; 
+}
