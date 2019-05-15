d767478839a2164c2b9ba938065a818974305231
yunh <haihai107@126.com>
2019-05-14 18:01:05 -0700

    to compare with file copies using read/write

--- 14.chapter/mapcp.c
+++ 14.chapter/mapcp.c
@@ -1,7 +1,16 @@
-#include "../apue.h" 
+//#include "../apue.h" 
 #include <fcntl.h> 
 #include <sys/mman.h> 
+#include <sys/stat.h> 
 #include <errno.h> 
+#include <unistd.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+
+#define FILE_MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+#define handle_error(msg) \
+    do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
 //#define TEST_SIGBUS
 #define USE_TRUNCATE
@@ -14,7 +23,7 @@ void sigbus (int signo)
 {
     printf ("caught a signal %d\n", signo); 
     if (ftruncate(g_fdout, g_len) == -1)
-        err_sys("ftruncate error"); 
+        handle_error("ftruncate"); 
     else 
         printf ("truncate file %d to new length %d\n", g_fdout, g_len); 
 }
@@ -26,7 +35,7 @@ int main (int argc, char *argv[])
     void *src, *dst; 
     struct stat statbuf; 
     if (argc != 3)
-        err_quit ("usage: %s <fromfile> <tofile>", argv[0]); 
+        handle_error ("wrong usage"); 
 
 #ifdef TEST_SIGBUS
     signal (SIGBUS, sigbus); 
@@ -34,14 +43,14 @@ int main (int argc, char *argv[])
 
     fdin = open (argv[1], O_RDONLY); 
     if (fdin < 0)
-        err_sys ("can't open %s for reading", argv[1]); 
+        handle_error ("open input"); 
 
     fdout = open (argv[2], O_RDWR | O_CREAT | O_TRUNC, FILE_MODE); 
     if (fdout < 0)
-        err_sys ("can't create %s for writing", argv[2]); 
+        handle_error ("create output"); 
 
     if (fstat (fdin, &statbuf) < 0)
-        err_sys ("fstat error"); 
+        handle_error ("fstat"); 
 
 #ifdef TEST_SIGBUS
     g_fdout = fdout; 
@@ -51,24 +60,25 @@ int main (int argc, char *argv[])
     // not portable than lseek + write
     // but still very portable
     if (ftruncate(fdout, statbuf.st_size) == -1)
-        err_sys("ftruncate error"); 
+        handle_error ("ftruncate"); 
 #  else
     if (lseek (fdout, statbuf.st_size - 1, SEEK_SET) == -1)
-        err_sys ("lseek error"); 
+        handle_error ("lseek"); 
 
     if (write (fdout, "", 1) != 1)
-        err_sys ("write error"); 
+        handle_error ("write"); 
 #  endif
 #endif
 
     src = mmap (0, statbuf.st_size, PROT_READ, MAP_SHARED, fdin, 0); 
     if (src == MAP_FAILED)
-        err_sys ("mmap error for input"); 
+        handle_error ("mmap input"); 
 
     dst = mmap (0, statbuf.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fdout, 0); 
     if (dst == MAP_FAILED)
-        err_sys ("mmap error for output"); 
+        handle_error ("mmap output"); 
 
     memcpy (dst, src, statbuf.st_size); 
+    printf ("copy over\n"); 
     exit (0); 
 }
---
+++ 14.chapter/rwcp.c
@@ -0,0 +1,51 @@
+//#include "../apue.h" 
+#include <fcntl.h> 
+#include <sys/mman.h> 
+#include <sys/stat.h> 
+#include <errno.h> 
+#include <unistd.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+
+#define FILE_MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+#define handle_error(msg) \
+    do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
+#define BUF_SIZE 8192
+
+int main (int argc, char *argv[])
+{
+    int fdin, fdout; 
+    char buf[BUF_SIZE] = { 0 }; 
+    struct stat statbuf; 
+    if (argc != 3)
+        handle_error ("wrong usage"); 
+
+    fdin = open (argv[1], O_RDONLY); 
+    if (fdin < 0)
+        handle_error ("open input"); 
+
+    fdout = open (argv[2], O_RDWR | O_CREAT | O_TRUNC, FILE_MODE); 
+    if (fdout < 0)
+        handle_error ("create output"); 
+
+    int rd = 0, wr = 0; 
+    while (1)
+    {
+        rd = read (fdin, buf, BUF_SIZE); 
+        if (rd < 0)
+            handle_error ("read input"); 
+        else if (rd == 0)
+            break; // reach end
+
+        wr = write (fdout, buf, rd); 
+        if (wr < rd)
+            handle_error ("write output"); 
+    }
+
+    printf ("copy over\n"); 
+    close (fdin); 
+    close (fdout); 
+    exit (0); 
+}
