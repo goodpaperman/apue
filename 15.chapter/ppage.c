ff949d1b0af3017c968eeeb1ddbd5668adf359ef
yunh <haihai107@126.com>
2019-06-20 18:07:44 -0700

    to test PAGER by popen/pclose

---
+++ 15.chapter/ppage.c
@@ -0,0 +1,38 @@
+#include "../apue.h" 
+#include <unistd.h> 
+#include <sys/wait.h> 
+
+#define PAGER "${PAGER:-more}"
+
+int main (int argc, char *argv[])
+{
+    int n = 0; 
+    char line[MAXLINE] = { 0 }; 
+    FILE *fpin = NULL, *fpout = NULL; 
+    if (argc != 2)
+        err_quit ("usage: ppage <pathname>"); 
+
+    fpin = fopen (argv[1], "r"); 
+    if (fpin == NULL)
+        err_sys ("can't open %s", argv[1]); 
+
+    fpout = popen (PAGER, "w"); 
+    if (fpout == NULL)
+        err_sys ("popen %s error", PAGER); 
+
+    while (fgets (line, MAXLINE, fpin) != NULL) { 
+        if (fputs (line, fpout) == EOF)
+            err_sys ("fputs error to pipe"); 
+    }
+
+    if (ferror (fpin))
+        err_sys ("fgets error"); 
+
+    int ret = pclose (fpout); 
+    if (ret == -1)
+        err_sys ("pclose error"); 
+    else 
+        printf ("worker return %d\n", ret); 
+
+    return 0; 
+}
