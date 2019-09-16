f8494e287bb29feb30a0819a22391ff9f3aca553
yunh <haihai107@126.com>
2019-09-15 19:19:03 -0700

    to see popen child process on stderr (need 2>&1 redirect at parent!)

--- 15.chapter/ptolower.c
+++ 15.chapter/ptolower.c
@@ -1,19 +1,31 @@
 #include "../apue.h" 
 #include <sys/wait.h> 
 
+//#define USE_STDERR 1
+
 int main (void)
 {
     char line[MAXLINE] = { 0 }; 
-    FILE *fp = popen ("./tolower", "r"); 
+    //FILE *fp = popen ("./tolower", "r"); 
+    FILE *fp = popen ("./tolower 2>&1", "r"); 
     if (fp == NULL)
         err_sys ("popen error"); 
 
     for (;;) { 
+#ifdef USE_STDERR
+        fputs ("prompt> ", stderr); 
+#else
         fputs ("prompt> ", stdout); 
         fflush (stdout); 
+#endif
         if (fgets (line, MAXLINE, fp) == NULL)
             break; 
+
+#ifdef USE_STDERR
+        if (fputs (line, stderr) == EOF)
+#else
         if (fputs (line, stdout) == EOF)
+#endif
             err_sys ("fputs error to pipe"); 
     }
 
--- 15.chapter/tolower.c
+++ 15.chapter/tolower.c
@@ -1,16 +1,24 @@
 #include "../apue.h" 
 #include <ctype.h> 
 
+#define USE_STDERR 1
+
 int main (void) 
 {
     int c; 
     while ((c = getchar ()) != EOF) { 
         if (isupper (c))
             c = tolower (c); 
+
+#ifdef USE_STDERR
+        if (fputc (c, stderr) == EOF)
+            err_sys ("output error"); 
+#else
         if (putchar (c) == EOF)
             err_sys ("output error"); 
         if (c == '\n')
             fflush (stdout); 
+#endif
     }
 
     return 0; 
