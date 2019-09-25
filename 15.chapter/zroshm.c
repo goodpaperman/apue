234d1424e6ba1a2e4631e72752a3d1a1e848e709
yunh <haihai107@126.com>
2019-09-25 02:07:48 -0700

    using shmget instead of mmap for integer update..

--- 15.chapter/zroshm.c
+++ 15.chapter/zroshm.c
@@ -1,10 +1,17 @@
 #include "../apue.h" 
 #include <fcntl.h> 
 #include <sys/mman.h> 
+#include <errno.h> 
 
-#define USE_ANON
-//#define USE_NULL
-//#define USE_PRIVATE
+#define USE_SHM
+
+#ifndef USE_SHM
+#  define USE_ANON
+//#  define USE_NULL
+//#  define USE_PRIVATE
+#else 
+#  include <sys/shm.h> 
+#endif
 
 #define NLOOPS 1000
 #define SIZE sizeof(long)
@@ -16,35 +23,69 @@ static int update (long *ptr)
 
 int main (void)
 {
-#ifdef USE_ANON
-    int fd = -1; 
-#else 
-#  ifdef USE_NULL
-    int fd = open ("/dev/null", O_RDWR); 
-#  else
-    int fd = open ("/dev/zero", O_RDWR); 
+    int ret = 0; 
+    void *area = 0; 
+#ifdef USE_SHM
+    int mode = 0666;  // 0; 
+    int flag = IPC_CREAT; 
+#  ifdef USE_EXCL
+    flag |= IPC_EXCL; 
 #  endif
+
+    int mid = shmget (IPC_PRIVATE, SIZE, flag | mode); 
+    if (mid < 0)
+        err_sys ("shmget for size %d failed", SIZE); 
+
+    printf ("create shared-memory %d with size %d ok\n", mid, SIZE); 
+
+#  ifdef USE_RND
+    flag = SHM_RND; 
+    area = 0x8004001; 
+    printf ("try addr 0x%08x\n", area); 
+#  endif
+
+#  ifdef USE_RDONLY
+    flag |= SHM_RDONLY; 
+    printf ("try open map readonly\n"); 
+#  endif
+
+   area = shmat (mid, area, flag); 
+   if (area == -1)
+       err_sys ("shmat failed, errno %d", errno); 
+
+   printf ("attach shared-memory at %p\n", area); 
+#else
+# ifdef USE_ANON
+    int fd = -1; 
+#  else 
+#    ifdef USE_NULL
+    int fd = open ("/dev/null", O_RDWR); 
+#    else
+    int fd = open ("/dev/zero", O_RDWR); 
+#    endif
     if (fd < 0)
         err_sys ("open error"); 
 
     printf ("open /dev/zero ok\n"); 
-#endif
+#  endif
 
     int flag = MAP_SHARED; 
-#ifdef USE_PRIVATE
+#  ifdef USE_PRIVATE
     flag = MAP_PRIVATE; 
-#endif
+#  endif
 
-#ifdef USE_ANON
+#  ifdef USE_ANON
     flag |= MAP_ANONYMOUS; 
-#endif
+#  endif
 
-    void *area = mmap (0, SIZE, PROT_READ | PROT_WRITE, flag, fd, 0); 
+    area = mmap (0, SIZE, PROT_READ | PROT_WRITE, flag, fd, 0); 
     if (area == MAP_FAILED)
         err_sys ("mmap error"); 
 
     printf ("map long from that file ok\n"); 
     close (fd); 
+#endif
+
     SYNC_INIT(); 
 
     int i=0, counter=0; 
@@ -84,6 +125,18 @@ int main (void)
 
         printf ("child exit\n"); 
     }
+
+#ifdef USE_SHM
+    shmdt (area); 
+    if (pid > 0)
+    {
+        ret = shmctl (mid, IPC_RMID, NULL); 
+        if (ret < 0)
+            err_sys ("shmctl to remove shared-memory failed"); 
+
+        printf ("remove that shared-memory\n"); 
+    }
+#endif 
 
     return 0; 
 }
