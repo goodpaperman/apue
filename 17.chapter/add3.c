bb18c96c77e13535852b500412fc3d45fc35a0f9
yunh <yunh@glodon.com>
2019-11-25 17:03:34 +0800

    edit on windows by gvim to avoid using vi on solaris :)

--- 17.chapter/add3.c
+++ 17.chapter/add3.c
@@ -1,44 +1,72 @@
+
+#include <signal.h>
 #include <unistd.h>
 #include <stdio.h> 
-#include <string.h> 
-#include <fcntl.h>
+#include <string.h>
 
 #define MAXLINE 128
 
+static void sig_pipe (int);
+
+int s_pipe (int fd[2])
+{
+	return (pipe(fd)); 
+}
+
 int main (void)
 {
-    int fdin, fdout, n, int1, int2; 
-    char line[MAXLINE]; 
-	fdin = open ("./pipe", O_RDWR); 
-	if (fdin < 0) {
-		printf ("open file pipe failed\n"); 
+    if (signal (SIGPIPE, sig_pipe) == SIG_ERR) {
+        printf ("signal error\n"); 
+		return 0; 
+	}
+ 
+	int fd[2];
+    if (s_pipe (fd) < 0) {
+        printf ("pipe error\n"); 
 		return 0; 
 	}
 
-	printf ("open file pipe ok, fd = %d\n", fdin); 
-	fdout = fdin; 
+	if (fattach (fd[1], "./pipe") < 0) {
+		printf ("fattach error\n"); 
+		return 0; 
+	}
 
-    while ((n = read (fdin, line, MAXLINE)) > 0) { 
+	printf ("attach to file pipe ok\n"); 
+
+	int n = 0; 
+    char line[MAXLINE]; 
+    close (fd[1]);
+    while ((n = read (fd[0], line, MAXLINE)) > 0) { 
         line[n] = 0; 
         if (sscanf (line, "%d%d", &int1, &int2) == 2) { 
             sprintf (line, "%d\n", int1 + int2); 
             n = strlen (line); 
-            if (write (fdout, line, n) != n) {
+            if (write (fd[0], line, n) != n) {
                 printf ("write error\n"); 
 				return 0; 
 			}
 			printf ("i am working on %s\n", line); 
         }
         else { 
-            if (write (fdout, "invalid args\n", 13) != 13) {
+            if (write (fd[0], "invalid args\n", 13) != 13) {
                 printf ("write msg error\n"); 
 				return 0; 
 			}
         }
     }
 
-	close (fdin); 
-	//close (fdout); 
-
+	if (fdetach ("./pipe") < 0) {
+		printf ("fdetach error\n"); 
+		return 0; 
+	}
+ 
+	printf ("detach from file pipe ok\n"); 
+	close (fd[0]); 
     return 0; 
+}
+
+static void sig_pipe (int signo)
+{
+    printf ("SIGPIPE caught\n"); 
+    exit (1); 
 }
