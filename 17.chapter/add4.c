6cfc6a0479d479dcc8fe0d7bef631865ac018a1c
yunhai <haihai107@126.com>
2019-11-26 16:08:16 +0800

    skeleton for connld

---
+++ 17.chapter/add4.c
@@ -0,0 +1,73 @@
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h> 
+#include <string.h>
+
+#define MAXLINE 128
+
+static void sig_pipe (int);
+
+int s_pipe (int fd[2])
+{
+	return (pipe(fd)); 
+}
+
+int main (void)
+{
+    if (signal (SIGPIPE, sig_pipe) == SIG_ERR) {
+        printf ("signal error\n"); 
+		return 0; 
+	}
+ 
+	int fd[2];
+    if (s_pipe (fd) < 0) {
+        printf ("pipe error\n"); 
+		return 0; 
+	}
+
+	if (fattach (fd[1], "./pipe") < 0) {
+		printf ("fattach error\n"); 
+		return 0; 
+	}
+
+	printf ("attach to file pipe ok\n"); 
+
+	int n = 0, int1 = 0, int2 = 0; 
+    char line[MAXLINE]; 
+    close (fd[1]);
+    while ((n = read (fd[0], line, MAXLINE)) > 0) { 
+        line[n] = 0; 
+		printf ("source: %s\n", line); 
+        if (sscanf (line, "%d%d", &int1, &int2) == 2) { 
+            sprintf (line, "%d\n", int1 + int2); 
+            n = strlen (line); 
+            if (write (fd[0], line, n) != n) {
+                printf ("write error\n"); 
+				return 0; 
+			}
+			printf ("i am working on %d + %d = %s\n", int1, int2, line); 
+        }
+        else { 
+            if (write (fd[0], "invalid args\n", 13) != 13) {
+                printf ("write msg error\n"); 
+				return 0; 
+			}
+        }
+    }
+
+	if (fdetach ("./pipe") < 0) {
+		printf ("fdetach error\n"); 
+		return 0; 
+	}
+ 
+	printf ("detach from file pipe ok\n"); 
+	close (fd[0]); 
+    return 0; 
+}
+
+static void sig_pipe (int signo)
+{
+    printf ("SIGPIPE caught\n"); 
+    exit (1); 
+}
---
+++ 17.chapter/padd4.c
@@ -0,0 +1,68 @@
+//#include "../apue.h"  
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h> 
+#include <fcntl.h>
+
+#define MAXLINE 128
+ 
+
+int main (int argc, char *argv[])
+{
+    if (argc != 1 && argc != 3) { 
+        printf ("usage: padd3 <int1> <int2>\n"); 
+        return 0; 
+    }
+
+    int fdin, fdout, n; 
+    char line[MAXLINE]; 
+	fdin = open ("./pipe", O_RDWR); 
+	if (fdin < 0) {
+		printf ("open file pipe failed\n"); 
+		return 0; 
+	}
+
+	printf ("open file pipe ok, fd = %d\n", fdin); 
+	fdout = fdin; 
+
+	if (argc == 1) {
+    	if ((n = read (STDIN_FILENO, line, MAXLINE)) < 0) {
+        	printf ("read error from stdin\n"); 
+			return 0; 
+		}
+	}
+	else {
+		sprintf (line, "%s %s", argv[1], argv[2]); 
+	}
+
+    n = strlen (line); 
+    if (write (fdout, line, n) != n){
+        printf ("write error to pipe\n"); 
+		return 0; 
+	}
+
+    if ((n = read (fdin, line, MAXLINE)) < 0) {
+        printf ("read error from pipe\n"); 
+		return 0; 
+	}
+
+    if (n == 0) { 
+        printf ("child closed pipe\n"); 
+		return 0; 
+    }
+
+    line[n] = 0; 
+    if (fputs (line, stdout) == EOF) {
+        printf ("fputs error\n"); 
+		return 0; 
+	}
+
+    if (ferror (stdin)) {
+        printf ("fputs error\n"); 
+		return 0; 
+	}
+
+	close (fdin);
+    return 0; 
+}
+
