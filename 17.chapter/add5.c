12970c4c704da686387263f04f7143dec7b9911b
yunh <haihai107@126.com>
2019-12-06 01:25:35 -0800

    to test unix socket listen/accept/connect

---
+++ 17.chapter/add5.c
@@ -0,0 +1,176 @@
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h> 
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h> 
+#include <errno.h>
+#include <time.h>
+#include <stddef.h> 
+#if 0
+#  include <stropts.h>
+#else
+#  include <sys/socket.h> 
+#  include <sys/un.h> 
+#endif
+
+#define QLEN 10
+#define STALE 30
+#define MAXLINE 128
+
+static void sig_pipe (int);
+
+//#define FIFO_MODE (S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH)
+
+int serv_listen (const char *name)
+{
+    int fd, len, err, rval; 
+    struct sockaddr_un un; 
+
+    if ((fd = socket (AF_UNIX, SOCK_STREAM, 0)) < 0) {
+        printf ("socket failed\n"); 
+        return -1; 
+    }
+
+    printf ("create socket ok\n"); 
+    unlink (name); 
+    memset (&un, 0, sizeof(un)); 
+    un.sun_family = AF_UNIX; 
+    strcpy (un.sun_path, name); 
+    len = offsetof (struct sockaddr_un, sun_path) + strlen (name); 
+
+    if (bind (fd, (struct sockaddr *)&un, len) < 0) {
+        err = errno; 
+        printf ("bind failed\n");  
+        rval = -2; 
+        goto errout; 
+    }
+
+    printf ("bind socket to path ok\n"); 
+    if (listen (fd, QLEN) < 0) { 
+        err = errno; 
+        printf ("listen failed\n"); 
+        rval = -3; 
+        goto errout; 
+    }
+
+    printf ("start listen on socket ok\n"); 
+    return fd; 
+errout:
+    close (fd); 
+    errno = err; 
+    return rval; 
+}
+
+int serv_accept (int listenfd, uid_t *uidptr)
+{
+    int clifd, len, err, rval; 
+    time_t staletime; 
+    struct sockaddr_un un; 
+    struct stat statbuf; 
+
+    len = sizeof (un); 
+    if ((clifd = accept (listenfd, (struct sockaddr *)&un, &len)) < 0) { 
+        printf ("accept failed\n"); 
+        return -1; 
+    }
+
+    len -= offsetof (struct sockaddr_un, sun_path); 
+    un.sun_path[len] = 0; 
+    printf ("accept %s ok\n", un.sun_path); 
+    if (stat (un.sun_path, &statbuf) < 0) {
+        err = errno; 
+        printf ("stat failed\n"); 
+        rval = -2; 
+        goto errout; 
+    }
+
+    printf ("stat file ok\n"); 
+    if (S_ISSOCK(statbuf.st_mode) == 0) { 
+        printf ("not a socket\n"); 
+        rval = -3; 
+        goto errout; 
+    }
+
+    printf ("yes, it is a socket\n"); 
+    if ((statbuf.st_mode & (S_IRWXG | S_IRWXO)) || 
+        (statbuf.st_mode & S_IRWXU) != S_IRWXU) { 
+        printf ("you have no right\n"); 
+        rval = -4; 
+        goto errout; 
+    }
+
+    printf ("and has rights\n"); 
+    staletime = time (NULL) - STALE; 
+    if (statbuf.st_atime < staletime || 
+        statbuf.st_ctime < staletime ||
+        statbuf.st_mtime < staletime) {
+        printf ("i-node is too old\n"); 
+        rval = -5; 
+        goto errout; 
+    }
+
+    printf ("and has nearly time\n"); 
+    if (uidptr != NULL)
+        *uidptr = statbuf.st_uid; 
+
+    unlink (un.sun_path); 
+    return clifd; 
+
+errout:
+    close (clifd); 
+    errno = err; 
+    return rval; 
+}
+
+int main (void)
+{
+    if (signal (SIGPIPE, sig_pipe) == SIG_ERR) {
+        printf ("signal error\n"); 
+		return 0; 
+	}
+ 
+	int listenfd = serv_listen ("./pipe"); 
+	if (listenfd < 0)
+		return 0; 
+
+	int acceptfd = 0; 
+	int n = 0, int1 = 0, int2 = 0; 
+	char line[MAXLINE]; 
+	uid_t uid = 0; 
+	while ((acceptfd = serv_accept (listenfd, &uid)) >= 0)
+	{
+		printf ("accept a client, fd = %d, uid = %ld\n", acceptfd, uid); 
+    	while ((n = read (acceptfd, line, MAXLINE)) > 0) { 
+    	    line[n] = 0; 
+			printf ("source: %s\n", line); 
+    	    if (sscanf (line, "%d%d", &int1, &int2) == 2) { 
+    	        sprintf (line, "%d\n", int1 + int2); 
+    	        n = strlen (line); 
+    	        if (write (acceptfd, line, n) != n) {
+    	            printf ("write error\n"); 
+					return 0; 
+				}
+				printf ("i am working on %d + %d = %s\n", int1, int2, line); 
+    	    }
+    	    else { 
+    	        if (write (acceptfd, "invalid args\n", 13) != 13) {
+    	            printf ("write msg error\n"); 
+					return 0; 
+				}
+    	    }
+    	}
+
+		close (acceptfd); 
+	}
+
+	close (listenfd); 
+    return 0; 
+}
+
+static void sig_pipe (int signo)
+{
+    printf ("SIGPIPE caught\n"); 
+    exit (1); 
+}
---
+++ 17.chapter/padd5.c
@@ -0,0 +1,129 @@
+//#include "../apue.h"  
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h> 
+#include <fcntl.h>
+#include <errno.h> 
+#include <stddef.h> 
+#if 0
+#  include <stropts.h>
+#else
+#  include <sys/socket.h> 
+#  include <sys/un.h> 
+#endif
+
+#define CLI_PATH "/var/tmp/"
+#define CLI_PERM S_IRWXU
+#define MAXLINE 128
+ 
+int cli_conn(const char *name)
+{
+    int fd, len, err, rval; 
+    struct sockaddr_un un; 
+
+    if ((fd = socket (AF_UNIX, SOCK_STREAM, 0)) < 0) { 
+        printf ("create socket failed\n"); 
+        return -1; 
+    }
+
+    printf ("create socket ok\n"); 
+    memset (&un, 0, sizeof (un)); 
+    un.sun_family = AF_UNIX; 
+    sprintf (un.sun_path, "%s%05d", CLI_PATH, getpid ()); 
+    len = offsetof (struct sockaddr_un, sun_path) + strlen (un.sun_path); 
+    unlink (un.sun_path); 
+
+    if (bind (fd, (struct sockaddr *)&un, len) < 0) { 
+        err = errno; 
+        printf ("bind failed\n"); 
+        rval = -2; 
+        goto errout; 
+    }
+
+    printf ("bind socket to temp file ok\n"); 
+    if (chmod (un.sun_path, CLI_PERM) < 0) { 
+        err = errno; 
+        printf ("chmod failed\n"); 
+        rval = -3; 
+        goto errout; 
+    }
+
+    printf ("change temp file mode ok\n"); 
+    memset (&un, 0, sizeof (un)); 
+    un.sun_family = AF_UNIX; 
+    strcpy (un.sun_path, name); 
+    len = offsetof (struct sockaddr_un, sun_path) + strlen (name); 
+    if (connect (fd, (struct sockaddr *)&un, len) < 0) {
+        err = errno; 
+        printf ("connect failed\n"); 
+        rval = -4; 
+        goto errout; 
+    }
+
+    printf ("connect to server ok\n"); 
+    return fd;
+errout:
+    close (fd); 
+    errno = err; 
+    return rval;
+}
+
+int main (int argc, char *argv[])
+{
+    if (argc != 1 && argc != 3) { 
+        printf ("usage: padd3 <int1> <int2>\n"); 
+        return 0; 
+    }
+
+    int fdin, fdout, n, m; 
+    char line[MAXLINE]; 
+	fdin = cli_conn ("./pipe"); 
+	if (fdin < 0)
+		return 0; 
+
+	printf ("open file pipe ok, fd = %d\n", fdin); 
+	fdout = fdin; 
+
+	if (argc == 1) {
+    	if ((n = read (STDIN_FILENO, line, MAXLINE)) < 0) {
+        	printf ("read error from stdin\n"); 
+			return 0; 
+		}
+	}
+	else {
+		sprintf (line, "%s %s", argv[1], argv[2]); 
+	}
+
+    n = strlen (line); 
+    if (write (fdout, line, n) != n){
+        printf ("write error to pipe\n"); 
+		return 0; 
+	}
+
+	strcat (line, " = "); 
+	m = strlen (line); 
+    if ((n = read (fdin, line + m, MAXLINE - m)) < 0) {
+        printf ("read error from pipe\n"); 
+		return 0; 
+	}
+
+    if (n == 0) { 
+        printf ("child closed pipe\n"); 
+		return 0; 
+    }
+
+    line[m+n] = 0; 
+    if (fputs (line, stdout) == EOF) {
+        printf ("fputs error\n"); 
+		return 0; 
+	}
+
+    if (ferror (stdin)) {
+        printf ("fputs error\n"); 
+		return 0; 
+	}
+
+	close (fdin);
+    return 0; 
+}
+
