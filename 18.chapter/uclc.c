95af5448a329fcaf12fed93da685e8b7a447ec5f
yunhai <haihai107@126.com>
2020-01-27 20:53:46 +0800

    to test OLCUC

--- 18.chapter/uclc.c
+++ 18.chapter/uclc.c
@@ -1,6 +1,8 @@
 #include "../apue.h"
 #include <termios.h> 
 
+//#define UC2LC
+
 int main (void)
 {
     struct termios term; 
@@ -10,16 +12,29 @@ int main (void)
     if (tcgetattr (STDIN_FILENO, &term) < 0)
         err_sys ("tcgetattr error"); 
 
+#ifdef UC2LC
     if (term.c_iflag & IUCLC) 
         printf ("no upper char to lower char convert\n"); 
     else
         printf ("has upper char to lower char convert\n"); 
 
     term.c_iflag |= IUCLC; 
+#else
+    if (term.c_oflag & OLCUC) 
+        printf ("no lower char to upper char convert\n"); 
+    else
+        printf ("has lower char to upper char convert\n"); 
+
+    term.c_oflag |= OLCUC; 
+#endif
 
     if (tcsetattr (STDIN_FILENO, TCSANOW, &term) < 0)
       err_sys ("tcsetattr error"); 
 
+#ifdef UC2LC
     printf ("upper char to lower char convert ok\n"); 
+#else
+    printf ("lower char to upper char convert ok\n"); 
+#endif
     return 0; 
 }
