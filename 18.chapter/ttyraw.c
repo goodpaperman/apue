2dd244c421edfcb40a9ac3e05783a4f176dd73f7
yunhai <haihai107@126.com>
2020-02-04 20:26:51 +0800

    to see raw mode and not exit (to use reset command)

---
+++ 18.chapter/ttyraw.c
@@ -0,0 +1,26 @@
+#include "../apue.h"
+#include "tty.h"
+
+int main (void)
+{
+  int i; 
+  char c; 
+  if (tty_raw (STDIN_FILENO) < 0)
+    err_sys ("tty_raw error"); 
+
+  printf ("Enter raw mode, terminate with DELETE\n"); 
+  while ((i = read (STDIN_FILENO, &c, 1)) == 1) {
+    if ((c &= 255) == 0177)
+      break; 
+
+    printf ("%o\n", c); 
+    //printf ("%d\n", c); 
+  }
+
+  // no tty_reset, to test reset cmd.
+  //if (tty_reset (STDIN_FILENO) < 0)
+  //  err_sys ("tty_reset error"); 
+
+  printf ("exit\n"); 
+  return 0; 
+}
