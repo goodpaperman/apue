d3227c6d585bdbe2948e9b53ac9eb85226ad8699
yunh <haihai107@126.com>
2020-02-13 21:00:57 -0800

    a patch

--- 19.chapter/slowout.c
+++ 19.chapter/slowout.c
@@ -4,12 +4,18 @@
 int main (void)
 {
     int i = 0; 
+    int tty = isatty (STDIN_FILENO); 
+    printf ("stdin isatty ? %s\n", tty ? "true" : "false"); 
+    tty = isatty (STDOUT_FILENO); 
+    printf ("stdout isatty ? %s\n", tty ? "true" : "false"); 
     while (i++ < 10)
     {
         printf ("turn %d\n", i); 
         sleep (1); 
         printf ("type any char to continue\n"); 
+#ifdef HAS_READ
         getchar (); 
+#endif
     }
     return 0; 
 }
