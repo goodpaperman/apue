22d475bbd8f1e52fe05023474c11f2447103cf74
yunh <haihai107@126.com>
2020-03-17 02:55:28 -0700

    walk around the whole DB

--- 20.chapter/db.c
+++ 20.chapter/db.c
@@ -5,28 +5,42 @@
 
 void Usage ()
 {
-    printf ("Usage: db filename [insert|delete|query] key [data]\n"); 
+    printf ("Usage: db filename insert key data\n"); 
+    printf ("       db filename query key\n"); 
+    printf ("       db filename delete key\n"); 
+    printf ("       db filename walk\n"); 
     exit (-1); 
 }
 
 int main (int argc, char *argv[])
 {
-    if (argc < 4) 
+    if (argc < 3) 
         Usage (); 
 
     char *filename = argv[1]; 
     char *action = argv[2]; 
-    char *key = argv[3]; 
+    char *key = NULL; 
     char *data = NULL; 
-    if (strcasecmp (action, "insert") == 0)
+    if (strcasecmp (action, "walk") == 0)
+    {
+    }
+    else if (strcasecmp (action, "delete") == 0 
+            || strcasecmp (action, "query") == 0)
+    {
+        if (argc < 4)
+            Usage (); 
+
+        key = argv[3]; 
+    }
+    else if (strcasecmp (action, "insert") == 0)
     {
         if (argc < 5)
             Usage (); 
 
+        key = argv[3]; 
         data = argv[4]; 
     }
-    else if (strcasecmp (action, "delete") != 0 
-            && strcasecmp (action, "query") != 0)
+    else 
     {
         Usage (); 
     }
@@ -45,6 +59,20 @@ int main (int argc, char *argv[])
     {
         ptr = db_fetch (db, key); 
         printf ("%s === %s\n", key, ptr); 
+        ptr = (char *)1; 
+    }
+    else  // walk
+    {
+        //db_rewind (); 
+        int n = 0; 
+        char buf[IDXLEN_MAX] = { 0 }; 
+        while ((ptr = db_nextrec(db, buf)) != NULL)
+        {
+            n ++; 
+            printf ("[%5d] %s --- %s\n", n, buf, ptr); 
+        }
+
+        printf ("walk done!\n"); 
         ptr = (char *)1; 
     }
 
