221c905b054596b1d5742a3a26c3ec40ca02737a
yunh <haihai107@126.com>
2020-04-09 02:24:44 -0700

    add build_qonstart

--- 21.chapter/printd.c
+++ 21.chapter/printd.c
@@ -85,6 +85,72 @@ void update_jobno (void)
         log_sys ("can't update job file"); 
 }
 
+void add_job (struct printreq *reqp, long jobid)
+{
+    struct job *jp; 
+    if ((jp = malloc (sizeof (struct job))) == NULL)
+        log_sys ("malloc failed"); 
+
+    memcpy (&jp->req, reqp, sizeof (struct printreq)); 
+    job->jobid = jobid; 
+    job->next = NULL; 
+
+    pthread_mutex_lock (&joblock); 
+    jp->prev = jobtail; 
+    if (jobtail == NULL)
+        jobhead = jp; 
+    else 
+        jobtail->next = jp; 
+
+    jobtail = jp; 
+    pthread_mutex_unlock (&joblock); 
+    pthread_cond_signal (&jobwait); 
+}
+
+void build_qonstart (void)
+{
+    int fd, err, nr; 
+    long jobid; 
+    DIR *dirp; 
+    struct dirent *entp; 
+    struct printreq req; 
+    char dname [FILENMSZ], fname [FILENMSZ]; 
+    sprintf (dname, "%s/%s", SPOOLDIR, REQDIR);  
+    if ((dirp = opendir (dname)) == NULL)
+        return; 
+
+    while ((entp = readdir (dirp)) != NULL) {
+        if (strcmp (entp->d_name, ".") == 0 || 
+                strcmp (entp->d_name, "..") == 0)
+            continue; 
+
+        sprintf (fname, "%s/%s/%s", SPOOLDIR, REQDIR, entp->d_name); 
+        if ((fd = open (fname, O_RDONLY)) < 0)
+            continue; 
+
+        nr = read (fd, &req, sizeof (struct printreq)); 
+        if (nr != sizeof (struct printreq)) {
+            if (nr  < 0)
+                err = errno; 
+            else 
+                err = EIO; 
+
+            close (fd); 
+            log_msg ("build_qonstart: can't read %s: %s", fname, strerror (err)); 
+            unlink (fname); 
+            sprintf (fname, "%s/%s/%s", SPOOLDIR, DATADIR, entp->d_name); 
+            unlink (fname); 
+            continue; 
+        }
+
+        jobid = atol (entp->d_name); 
+        log_msg ("adding job %ld to queue", jobid); 
+        add_job (&req, jobid); 
+    }
+
+    closedir (dirp); 
+}
+
 int main (int argc, char *argv[])
 {
     pthread_t tid; 
