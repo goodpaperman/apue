61d9e542299781dd5b7337ad0faa006ee1d2fe5a
yunhai <haihai107@126.com>
2020-07-02 16:34:00 +0800

    to see lseek on pipe failed with ESPIPE

--- 03.chapter/seekable.c
+++ 03.chapter/seekable.c
@@ -1,12 +1,13 @@
 #include <stdio.h> 
 #include <unistd.h> 
+#include <errno.h>
 
 int main (int argc, char *argv[])
 {
   int ret = 0; 
   ret = lseek (STDIN_FILENO, 0, SEEK_CUR); 
   if (ret == -1)
-    printf ("cannot seek\n"); 
+    printf ("cannot seek, errno %d\n", errno); 
   else 
     printf ("seekable!\n"); 
 
