568a447ef35555429a1f4e8da1483916e914a012
yunhai <haihai107@126.com>
2020-12-09 16:12:07 +0800

    optimize

--- 04.chapter/user_init.sh
+++ 04.chapter/user_init.sh
@@ -4,10 +4,17 @@ groupadd share
 echo "create group ok"
 
 useradd lippmann -G share,common
-useradd -g share steven
-useradd -g common caveman
-useradd -g common paperman
+useradd steven -G share
+useradd caveman -G common
+useradd paperman -G common
 echo "create user ok"
+
+groups lippmann steven caveman paperman
+echo "show user and their group ok"
+
+groupdel common
+groupdel share
+echo "delete group ok"
 
 groups lippmann steven caveman paperman
 echo "show user and their group ok"
@@ -23,8 +30,4 @@ rm -rf /home/steven
 rm -rf /home/caveman
 rm -rf /home/paperman
 echo "remve user home dir ok"
-
-groupdel common
-groupdel share
-echo "delete group ok"
 
