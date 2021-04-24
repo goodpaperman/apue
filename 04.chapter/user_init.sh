c3875da9b4026079a911adf068467b6cdf041c21
yunh <haihai107@126.com>
2021-04-24 19:43:57 +0800

    rename lippmann to lippman

--- 04.chapter/user_init.sh
+++ 04.chapter/user_init.sh
@@ -3,29 +3,29 @@ groupadd common
 groupadd share
 echo "create group ok"
 
-useradd lippmann -G share,common
+useradd lippman -G share,common
 useradd steven -G share
 useradd caveman -G common
 useradd paperman -G common
 echo "create user ok"
 
-groups lippmann steven caveman paperman
+groups lippman steven caveman paperman
 echo "show user and their group ok"
 
 groupdel common
 groupdel share
 echo "delete group ok"
 
-groups lippmann steven caveman paperman
+groups lippman steven caveman paperman
 echo "show user and their group ok"
 
-userdel lippmann
+userdel lippman
 userdel steven
 userdel caveman
 userdel paperman
 echo "delete user ok"
 
-rm -rf /home/lippmann
+rm -rf /home/lippman
 rm -rf /home/steven
 rm -rf /home/caveman
 rm -rf /home/paperman
