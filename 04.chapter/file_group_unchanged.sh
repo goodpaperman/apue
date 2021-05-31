56263eb6849420d07956a99b1daf007312a5af49
yunh <haihai107@126.com>
2021-05-31 14:18:14 +0800

    add missing file

---
+++ 04.chapter/file_group_unchanged.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+echo "switch to user lippman"
+# ensure new user can create file
+cd /tmp
+
+# lippman belongs to lippman/share/men
+# can change between them
+touch this_is_a_test_file
+ls -lh this_is_a_test_file
+
+# change current owner's group
+sudo usermod -g share lippman
+groups lippman
+echo "show user and their group ok"
+
+touch this_is_a_demo_file
+ls -lh this_is_a*
+
+rm this_is_a*
+echo "remove testing file ok"
+
+# change group back
+sudo usermod -g lippman lippman
+cd -
+echo "test file group unchange over"
