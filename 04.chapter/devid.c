7cff7b5c9e5132747965af70bd3bf834c92d5a9c
yunhai <haihai107@126.com>
2022-03-20 20:26:08 +0800

    a patch

--- 04.chapter/devid.c
+++ 04.chapter/devid.c
@@ -12,11 +12,19 @@ int main (int argc, char *argv[])
   for (i=1; i<argc; ++ i)
   {
     printf ("%s: ", argv[i]); 
+#if 0
+    if (lstat (argv[i], &buf) < 0)
+    {
+      err_ret ("lstat error"); 
+      continue; 
+    }
+#else
     if (stat (argv[i], &buf) < 0)
     {
       err_ret ("stat error"); 
       continue; 
     }
+#endif
 
     printf ("dev = %d/%d", major(buf.st_dev), minor(buf.st_dev)); 
     if (S_ISCHR(buf.st_mode) || S_ISBLK(buf.st_mode))
