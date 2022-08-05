40dc95d8224ccb9c6f9697c808d3b213f3d304c4
yunhai01 <yunhai01@baidu.com>
2022-08-05 20:56:16 +0800

    new fwide[w] to check more things

--- 05.chapter/fwide.c
+++ 05.chapter/fwide.c
@@ -1,41 +1,63 @@
 #include "../apue.h"
 #include <wchar.h> 
 
+void do_fwide (FILE* fp, int wide)
+{
+  if (wide > 0)
+      fwprintf (fp, L"do fwide %d\n", wide); 
+  else
+      fprintf (fp, "do fwide %d\n", wide); 
+}
+
+/**
+ *@param: wide
+ *   -1 : narrow
+ *   1  : wide
+ *   0  : undetermine
+ */
+void set_fwide (FILE* fp, int wide)
+{
+  int ret = fwide (fp, wide); 
+  printf ("old wide = %d, new wide = %d\n", ret, wide); 
+}
+
+void get_fwide (FILE* fp)
+{
+    set_fwide (fp, 0); 
+}
+
 int main (int argc, char* argv[])
 {
-  int ret = 0; 
+  int towide = 0; 
   FILE* fp = fopen ("abc.txt", "w+"); 
   if (fp == 0)
     err_sys ("fopen failed"); 
 
-#if 1
-  ret = fwide (fp, 0); 
-#elif defined(USE_WCHAR)
-  ret = fwide (fp, -1); 
+#if defined (USE_WCHAR)
+  towide = 1;
 #else
-  ret = fwide (fp, 1); 
+  towide = -1;  
 #endif
-  printf ("fwide = %d\n", ret); 
-#ifdef USE_WCHAR
-  fwprintf (fp, L"fwide = %d\n", ret); 
+
+#if defined (USE_EXPLICIT_FWIDE)
+  // set wide explicitly
+  set_fwide (fp, towide); 
 #else
-  fprintf (fp, "fwide = %d\n", ret); 
+  // set wide automatically by s[w]printf
+  do_fwide (fp, towide); 
 #endif 
 
-#if 1
-  ret = fwide (fp, 0); 
-#elif defined(USE_WCHAR)
-  ret = fwide (fp, -1); 
-#else 
-  ret = fwide (fp, 1); 
-#endif 
-  printf ("fwide = %d\n", ret); 
+  get_fwide (fp); 
 
-#ifdef USE_WCHAR
-  fwprintf (fp, L"fwide = %d\n", ret); 
-#else
-  fprintf (fp, "fwide = %d\n", ret); 
-#endif 
+  // test set fwide after wide determined
+  set_fwide (fp, towide > 0 ? -1 : 1); 
+
+  get_fwide (fp); 
+
+  // test output with same wide
+  do_fwide (fp, towide); 
+  // test output with different wide
+  do_fwide (fp, towide > 0 ? -1 : 1); 
 
   fclose (fp); 
   return 0; 
