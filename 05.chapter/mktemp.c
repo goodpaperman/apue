968c31df9da38edd95233c5d4a2a0ae11fd22d96
yunhai <haihai107@126.com>
2022-09-03 18:01:00 +0800

    test mktemp with errno XXXXXX position

--- 05.chapter/mktemp.c
+++ 05.chapter/mktemp.c
@@ -14,8 +14,13 @@ int main (int argc, char *argv[])
   unlink (line); 
 
   strcpy (line, "/tmp/that_is_a_temp_file_XXXXXX"); 
+  printf ("%p: %s\n", line, line); 
   char *ptr = mktemp (line); 
-  printf ("%s, errno = %d\n", ptr, errno); 
+  printf ("%p: %s, errno = %d\n", ptr, ptr, errno); 
+
+  strcpy (line, "/tmp/XXXXXX_that_is_a_temp_file"); 
+  ptr = mktemp (line); 
+  printf ("%p: %s, errno = %d\n", ptr, ptr, errno); 
   return 0; 
 }
 
