aa9e1e2f54c6ce9269ebc336080679350a8e0341
yunh <haihai107@126.com>
2022-09-06 14:18:39 +0800

    optimize driver script of fdopen_t

--- 05.chapter/fdopen_t.sh
+++ 05.chapter/fdopen_t.sh
@@ -1,74 +1,11 @@
 #! /bin/sh
 
-./fdopen_t abc.txt r r
-sleep 1
-./fdopen_t abc.txt r r+
-sleep 1
-./fdopen_t abc.txt r w
-sleep 1
-./fdopen_t abc.txt r w+
-sleep 1
-./fdopen_t abc.txt r a
-sleep 1
-./fdopen_t abc.txt r a+
-sleep 1
-./fdopen_t abc.txt w r
-sleep 1
-./fdopen_t abc.txt w r+
-sleep 1
-./fdopen_t abc.txt w w
-sleep 1
-./fdopen_t abc.txt w w+
-sleep 1
-./fdopen_t abc.txt w a
-sleep 1
-./fdopen_t abc.txt w a+
-sleep 1
-./fdopen_t abc.txt a r
-sleep 1
-./fdopen_t abc.txt a r+
-sleep 1
-./fdopen_t abc.txt a a
-sleep 1
-./fdopen_t abc.txt a a+
-sleep 1
-./fdopen_t abc.txt a w
-sleep 1
-./fdopen_t abc.txt a w+
-sleep 1
-./fdopen_t abc.txt r+ w
-sleep 1
-./fdopen_t abc.txt r+ r
-sleep 1
-./fdopen_t abc.txt r+ r+
-sleep 1
-./fdopen_t abc.txt r+ w+
-sleep 1
-./fdopen_t abc.txt r+ a
-sleep 1
-./fdopen_t abc.txt r+ a+
-sleep 1
-./fdopen_t abc.txt w+ r
-sleep 1
-./fdopen_t abc.txt w+ r+
-sleep 1
-./fdopen_t abc.txt w+ w
-sleep 1
-./fdopen_t abc.txt w+ w+
-sleep 1
-./fdopen_t abc.txt w+ a
-sleep 1
-./fdopen_t abc.txt w+ a+
-sleep 1
-./fdopen_t abc.txt a+ r
-sleep 1
-./fdopen_t abc.txt a+ r+
-sleep 1
-./fdopen_t abc.txt a+ w
-sleep 1
-./fdopen_t abc.txt a+ w+
-sleep 1
-./fdopen_t abc.txt a+ a
-sleep 1
-./fdopen_t abc.txt a+ a+
-sleep 1
+oflags=("r" "w" "a" "r+" "w+" "a+")
+modes=("r" "r+" "w" "w+" "a" "a+")
+for oflag in ${oflags[@]}
+do
+    for mode in ${modes[@]}
+    do
+        ./fdopen_t abc.txt ${oflag} ${mode}
+    done
+done
