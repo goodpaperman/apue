136c85dc3330932a5b6befdf43d80f35e198a346
yunhai01 <yunhai01@baidu.com>
2023-04-02 18:24:16 +0800

    group strftime format options

--- 06.chapter/timeprintf.c
+++ 06.chapter/timeprintf.c
@@ -32,47 +32,61 @@ main (int argc, char *argv[])
 
   struct tm *t = localtime (&now); 
   print_tm (t); 
+  printf ("year group:\n");
+  my_strftime ("%Y", t); 
+  my_strftime ("%C", t); 
+  my_strftime ("%y", t); 
+  my_strftime ("%G", t); 
+  my_strftime ("%g", t); 
+
+  printf ("month group:\n"); 
+  my_strftime ("%m", t); 
+  my_strftime ("%b", t); 
+  my_strftime ("%h", t); 
+  my_strftime ("%B", t); 
+
+  printf ("day group:\n");
+  my_strftime ("%w", t); 
+  my_strftime ("%u", t); 
   my_strftime ("%a", t); 
   my_strftime ("%A", t); 
-  my_strftime ("%b", t); 
-  my_strftime ("%B", t); 
-  my_strftime ("%c", t); 
-  my_strftime ("%C", t); 
   my_strftime ("%d", t); 
-  my_strftime ("%D", t); 
   my_strftime ("%e", t); 
-  my_strftime ("%F", t); 
-  my_strftime ("%g", t); 
-  my_strftime ("%G", t); 
-  my_strftime ("%h", t); 
-  my_strftime ("%H", t); 
-  my_strftime ("%I", t); 
-  my_strftime ("%k", t); 
-  my_strftime ("%l", t); 
   my_strftime ("%j", t); 
-  my_strftime ("%m", t); 
+
+  printf ("week group:\n"); 
+  my_strftime ("%U", t); 
+  my_strftime ("%W", t); 
+  my_strftime ("%V", t); 
+
+  printf ("date group\n"); 
+  my_strftime ("%D", t); 
+  my_strftime ("%x", t); 
+  my_strftime ("%F", t); 
+
+  printf ("time group\n"); 
+  my_strftime ("%H", t); 
+  my_strftime ("%k", t); 
+  my_strftime ("%I", t); 
+  my_strftime ("%l", t); 
   my_strftime ("%M", t); 
-  my_strftime ("%n", t); 
+  my_strftime ("%S", t); 
+  my_strftime ("%T", t); 
+  my_strftime ("%X", t); 
+  my_strftime ("%R", t); 
   my_strftime ("%p", t); 
   my_strftime ("%P", t); 
   my_strftime ("%r", t); 
-  my_strftime ("%R", t); 
-  my_strftime ("%S", t); 
+  my_strftime ("%c", t); 
   my_strftime ("%s", t); 
-  my_strftime ("%t", t); 
-  my_strftime ("%T", t); 
-  my_strftime ("%u", t); 
-  my_strftime ("%U", t); 
-  my_strftime ("%V", t); 
-  my_strftime ("%w", t); 
-  my_strftime ("%W", t); 
-  my_strftime ("%x", t); 
-  my_strftime ("%X", t); 
-  my_strftime ("%y", t); 
-  my_strftime ("%Y", t); 
+
+  printf ("timezone group\n"); 
   my_strftime ("%z", t); 
   my_strftime ("%Z", t); 
+
+  printf ("common group\n"); 
+  my_strftime ("%n", t); 
+  my_strftime ("%t", t); 
   my_strftime ("%%", t); 
-  my_strftime ("%+", t); 
   return 0; 
 }
