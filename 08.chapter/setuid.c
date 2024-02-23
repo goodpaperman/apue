0141ff9d828961810bf58e67239898443514a0fa
yunhai01 <yunhai01@baidu.com>
2024-02-23 20:17:49 +0800

    test setuid/seteuid/setreuid

---
+++ 08.chapter/setuid.c
@@ -0,0 +1,63 @@
+#include "../apue.h"
+#include <sys/types.h> 
+#include <sys/file.h> 
+#include <sys/stat.h> 
+#include <unistd.h> 
+
+void print_ids ()
+{
+#if 0
+    char buf[1024]={ 0 }; 
+    sprintf (buf, "ps -p %d -eo ppid,pid,ruid,euid,suid,cmd", getpid()); 
+    system(buf); 
+#else
+    uid_t ruid = 0; 
+    uid_t euid = 0; 
+    uid_t suid = 0;
+    int ret = getresuid (&ruid, &euid, &suid); 
+    if (ret == 0)
+        printf ("%d: ruid %d, euid %d, suid %d\n", getpid(), ruid, euid, suid); 
+    else 
+        err_sys ("getresuid"); 
+#endif
+}
+
+int main (int argc, char *argv[])
+{
+    if (argc == 2)
+    {
+        char* uid=argv[1]; 
+        int ret = setuid(atol(uid)); 
+        if (ret != 0)
+            err_sys ("setuid"); 
+
+        print_ids(); 
+    }
+    else if (argc == 3)
+    {
+        char* ruid=argv[1]; 
+        char* euid=argv[1]; 
+        int ret = setreuid(atol(ruid), atol(euid)); 
+        if (ret != 0)
+            err_sys ("setreuid"); 
+
+        print_ids(); 
+    }
+    else if (argc > 1)
+    {
+        char* uid=argv[1]; 
+        int ret = seteuid(atol(uid)); 
+        if (ret != 0)
+            err_sys ("seteuid"); 
+
+        print_ids(); 
+    }
+    else 
+    {
+        print_ids(); 
+    }
+
+    return 0; 
+}
+
+
